0f333ccc8213dd2fa0687f30732156892f9239c1
Zhang Boxuan <zhangboxuan2008@hotmail.com>
2013-03-10 20:06:56 +0800

    通过sharedpreferences实现保存密码，使用AES加密

--- src/com/webdisk/activity/LoginActivity.java
+++ src/com/webdisk/activity/LoginActivity.java
@@ -9,39 +9,50 @@ import org.tmatesoft.svn.core.wc.SVNRevision;
 import com.webdisk.R;
 import com.webdisk.application.SVNApplication;
 import com.webdisk.model.Connection;
+import com.webdisk.util.AESUtil;
 
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 
+@SuppressLint("WorldReadableFiles")
 public class LoginActivity extends Activity {
 	
 	
 	private static final String TAG = "LoginActivity";
+	private static final String mPREFERENCES = "userInfo";
 	
+	private static final int LOGIN_START = 98;
 	private static final int LOGIN_SUCCESS = 101;
 	private static final int LOGIN_AUTH_ERROR = 100;
 	private static final int LOGIN_NO_CONNECTION = 99;
 	
-	
 	private SVNApplication app;
+	private SharedPreferences sharedPreferences;
 
 	private EditText et_accountName;
 	private EditText et_psw;
 	private Button btn_login;
 	private Button btn_regNow;
+	private CheckBox cb_savePsw;
+	private CheckBox cb_autoLogin;
 	
 	private SVNURL fileUrl = null;
 	
@@ -66,6 +77,8 @@ public class LoginActivity extends Activity {
 					
 					Toast.makeText(LoginActivity.this, R.string.login_auth_error, Toast.LENGTH_SHORT).show();
 					et_psw.setText("");
+					btn_login.setText(R.string.login);
+					btn_login.setEnabled(true);
 					break;
 				}
 				case LOGIN_NO_CONNECTION:
@@ -74,6 +87,14 @@ public class LoginActivity extends Activity {
 					
 					Toast.makeText(LoginActivity.this, R.string.login_no_connection, Toast.LENGTH_SHORT).show();
 					et_psw.setText("");
+					btn_login.setText(R.string.login);
+					btn_login.setEnabled(true);
+					break;
+				}
+				case LOGIN_START:
+				{
+					btn_login.setText(R.string.logining);
+					btn_login.setEnabled(false);
 					break;
 				}
 			}
@@ -83,17 +104,41 @@ public class LoginActivity extends Activity {
 	};
 	
 	
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         
         this.app = (SVNApplication)getApplication();
+        sharedPreferences = this.getSharedPreferences(mPREFERENCES,Context.MODE_WORLD_READABLE);  
         
         et_accountName = (EditText)findViewById(R.id.et_accoutName);
         et_psw = (EditText)findViewById(R.id.et_psw);
         btn_login = (Button)findViewById(R.id.btn_login);
         btn_regNow = (Button)findViewById(R.id.btn_regNow);
+        cb_savePsw = (CheckBox)findViewById(R.id.cb_savePsw);
+        cb_autoLogin = (CheckBox)findViewById(R.id.cb_autoLogin);
+        
+        //读取SharedPreference中保存的设置
+        et_accountName.setText(sharedPreferences.getString("userName", ""));
+        if (sharedPreferences.getBoolean("SAVEPSW_ISCHECK", false)) //之前设置记住密码
+        {
+//        	et_psw.setText(sharedPreferences.getString("password", ""));
+        	
+        	String pswDecrypted = "";
+        	try
+			{
+				pswDecrypted = AESUtil.decrypt(sharedPreferences.getString("userName", ""), sharedPreferences.getString("password", ""));
+				Log.i(TAG, "AES解密结果：" + pswDecrypted);
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+				Log.i(TAG, "AES解密错误");
+			}
+        	et_psw.setText(pswDecrypted);
+        	cb_savePsw.setChecked(true);
+        }
+        
         
         //为两个按钮设置按下效果
         btn_login.setOnTouchListener(new Button.OnTouchListener()
@@ -140,6 +185,29 @@ public class LoginActivity extends Activity {
 				final String password = et_psw.getText().toString();
 				final String cachePath = Environment.getExternalStorageDirectory() + "/Webdisk/cache/";// TODO 此路径需要修改
 				
+				//在SharedPreference中保存密码保存这次的用户名
+				Editor editor = sharedPreferences.edit();
+		        editor.putString("userName", userName).commit();
+				//如果勾选记住密码，在SharedPreference中保存密码
+				if(cb_savePsw.isChecked())
+				{
+//					editor.putString("password", password).commit();
+					try
+					{
+						editor.putString("password", AESUtil.encrypt(userName, password)).commit();
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+						Log.i(TAG, "AES加密错误");
+					}
+	                editor.putBoolean("SAVEPSW_ISCHECK", true).commit(); 
+				}
+				else//如果没有勾选，清楚之前保存的项目
+				{
+					editor.putString("password", "").commit();
+	                editor.putBoolean("SAVEPSW_ISCHECK", false).commit(); 
+				}
+				
 				Log.i(TAG, "cachePath=" + cachePath);
 				
 				final File cacheFolder = new File(cachePath);
@@ -148,8 +216,6 @@ public class LoginActivity extends Activity {
 					cacheFolder.mkdir();
 				}
 					
-				
-				
 				try
 				{
 					// TODO 需要修改为邮箱的映射文件
@@ -197,6 +263,10 @@ public class LoginActivity extends Activity {
 					@Override
 					public void run()
 					{
+						Message loginStartMsg = new Message();
+						loginStartMsg.what = LOGIN_START;
+						mHandler.sendMessage(loginStartMsg);
+						
 						Connection thisConnection = new Connection();
 						
 						// prep data for connection
